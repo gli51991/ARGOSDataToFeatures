ee50dcb6c73def9ea1c1185aeab474a1766bfdf7
gli <51991+gli@users.noreply.github.com>
2018-10-15 12:35:41 -0400

    Gets Tag ID attribute

--- importARGOS.py
+++ importARGOS.py
@@ -16,3 +16,20 @@ import sys, os, arcpy
 # Set input variables (Hard-wired)
 inputFile = 'V:/ARGOSTracking/Data/ARGOSData/1997dg.txt'
 outputFC = "V:/ARGOSTracking/Scratch/ARGOStrack.shp"
+
+# Open the ARGOS data file for reading
+inputFileObj = open(inputFile, 'r')
+
+# Get the first line of data, so we can use a while loop
+lineString = inputFileObj.readline()
+while lineString:
+
+    # Set code to run only if the line contains the string "Date: "
+    if ("Date :" in lineString):
+        lineData = lineString.split()
+        print(lineData[0])
+        break
+
+    
+    # Get the next line
+    lineString = inputFileObj.readline()
